26ea4e9d8fea59d4453618bf6391f5934ce03e64
Alexis Viscogliosi <alexis.viscogliosi@outlook.fr>
2025-03-31 20:26:49 +0200

    refactor: simplify HomeController by removing unused methods and optimizing index action

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,69 +1,4 @@
 class HomeController < ApplicationController
-  def index
-    @product = Product
-                 .includes(:product_variants)
-                 .order(id: :desc)
-                 .first
-
-    @product.product_variants.sort_by { |variant| variant.variants_slug }
-
-    @images = {
-      product_images: @product.ordered_images.map { |image| to_image(@product.id, image) }
-    }
-
-    @product.product_variants.each do |variant|
-      variant.ordered_images.each do |image|
-        @images[variant.variants_slug] ||= []
-        @images[variant.variants_slug] << to_image(variant.id, image)
-      end
-    end
-
-    @default_variant = find_default_variant(@product.product_variants.to_a, params)
-
-    @product.faq.sort_by { |element| element["position"] }
-    @product.specifications.sort_by { |element| element["position"] }
-    @reviews = Review.order(created_at: :desc)
-    @setting = Setting.first
-
-    preview_image_path = ActionController::Base.helpers.image_path("preview_image.png")
-
-    og = JSON.parse(@setting.og)
-    twitter = JSON.parse(@setting.twitter)
-
-    og["type"] = "product" unless og["type"].present?
-    og["title"] = "#{@product.name} | #{@setting.shop_name}" unless og["title"]
-    og["description"] = @product.short_description unless og["description"]
-    og["url"] = Rails.application.routes.url_helpers.home_url unless og["url"]
-    og["image"] = preview_image_path unless og["image"]
-    og["image_width"] = "600" unless og["image_width"]
-    og["image_height"] = "450" unless og["image_height"]
-    og["site_name"] = @setting.shop_name unless og["site_name"]
-    og["price"] = {
-      amount: (@product.base_price / 100).to_s,
-      currency: "EUR"
-    } unless og["price"]
-    og["availability"] = @product.in_stock? ? "instock" : "oos" unless og["availability"]
-
-    # Configuration des meta tags Twitter
-    twitter["card"] = "product" unless twitter["card"].present?
-    twitter["title"] = "#{@product.name} | #{@setting.shop_name}" unless twitter["title"]
-    twitter["description"] = @product.short_description unless twitter["description"]
-    twitter["image"] = preview_image_path unless twitter["image"]
-    twitter["site"] = Rails.application.routes.url_helpers.home_url unless twitter["site"]
-    twitter["data1"] = (@product.base_price / 100).to_s + " EUR" unless twitter["data1"]
-    twitter["label1"] = "Prix" unless twitter["label1"]
-    twitter["data2"] = @product.in_stock? ? "En stock" : "Rupture de stock" unless twitter["data2"]
-    twitter["label2"] = "Disponibilité" unless twitter["label2"]
-
-    set_meta_tags(
-      description: @setting.short_desc,
-      keywords: @setting.keywords,
-      image_src: preview_image_path,
-      og: og,
-      twitter: twitter
-    )
-  end
-
   def api_settings
     setting = Setting.first
     render json: {
@@ -144,11 +79,6 @@ class HomeController < ApplicationController
     }
   end
 
-  def sales_terms
-    @setting = Setting.first
-    @cgv = replace_variables(@setting.cgv)
-  end
-
   def api_sales_terms
     @setting = Setting.first
     @cgv = replace_variables(@setting.cgv)
@@ -159,11 +89,6 @@ class HomeController < ApplicationController
     }
   end
 
-  def legal_notices
-    @setting = Setting.first
-    @legal_notices = replace_variables(@setting.legal_notices)
-  end
-
   def api_legal_notices
     @setting = Setting.first
     @legal_notices = replace_variables(@setting.legal_notices)
@@ -172,9 +97,6 @@ class HomeController < ApplicationController
       setting: @setting,
       value: @legal_notices
     }
-  end
-
-  def success
   end
 
   private
